1dbe565e700f3a5468b60a348e1c0bd6d04b283d
unknown <muhammadismail2418@gmail.com>
2024-02-23 19:23:29 +0700

    pull from main

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import numpy as np
 parser = argparse.ArgumentParser(description='MaelNet for Time Series Anomaly Detection')
 
 # basic config
-parser.add_argument('--is_training', type=int, default=1, help='status')
+parser.add_argument('--is_training', type=int, default=0, help='status')
 parser.add_argument('--model_id', type=str, default='DCDetector_MSL', help='model id')
 parser.add_argument('--model', type=str, default='DCDetector',
                     help='model name, options: [MaelNet]')

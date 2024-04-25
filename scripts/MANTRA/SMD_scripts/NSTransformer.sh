cacb426a501378e4d295d275dd6f09418b284dbc
mail2418 <muhammadismail2418@gmail.com>
2024-04-26 06:25:04 +0700

    feat:add new testing model

---
+++ scripts/MANTRA/SMD_scripts/NSTransformer.sh
@@ -0,0 +1,20 @@
+py -u run_mantra.py \
+  --is_training 1 \
+  --root_path ./dataset/SMD/ \
+  --model_id NSTransformerB1_NSTransformerS1_SMD_MSE_NO_SL\
+  --model NSTransformerB1 \
+  --slow_model NSTransformerS1 \
+  --data SMD \
+  --e_layers 2 \
+  --anomaly_ratio 0.6 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 38 \
+  --dec_in 38 \
+  --c_out 38 \
+  --d_model 512 \
+  --gpu 0 \
+  --des 'Exp_h256_l2' \
+  --p_hidden_dims 128 128 \
+  --p_hidden_layers 2 \
+  --itr 1 &
